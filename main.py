3582bd7b131c9c7351def383f7c9344e159faad4
JoseLuisSR <jose.luis.sastoque.rey@gmail.com>
2026-08-06 14:18:16 -0500

    Add gunicorn to deploy flask app.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from src.infrastructure.web.app import create_app
 
+app: Flask = create_app()
+
 if __name__ == "__main__":
-    app: Flask = create_app()
     app.run(debug=True)
